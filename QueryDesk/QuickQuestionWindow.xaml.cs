36f61a981580c43e6e2ca5ad462d4faeb61ff3d2
partouf <partouf@gmail.com>
2014-05-14 01:28:53 +0200

    form

---
+++ QueryDesk/QuickQuestionWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QueryDesk
+{
+    /// <summary>
+    /// Interaction logic for QuickQuestionWindow.xaml
+    /// </summary>
+    public partial class QuickQuestionWindow : Window
+    {
+        public string Question
+        {
+            get { return (string)lblQuestion.Content; }
+            set { lblQuestion.Content = value; }
+        }
+        public Func<string> Answer = () => "";
+
+        public QuickQuestionWindow()
+        {
+            InitializeComponent();
+
+            Answer = () => edAnswer.Text;
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+    }
+}
