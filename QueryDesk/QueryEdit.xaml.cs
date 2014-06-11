35ef62c3891c98e618ea24a11e28ed7c7aa1d4ed
martinvd <martinvdpub@gmail.com>
2014-06-11 23:33:17 +0200

    Namespace list order

--- QueryDesk/QueryEdit.xaml.cs
+++ QueryDesk/QueryEdit.xaml.cs
@@ -12,9 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Xml;
+using System.Reflection;
 using ICSharpCode.AvalonEdit.Highlighting;
 using ICSharpCode.AvalonEdit.Highlighting.Xshd;
-using System.Reflection;
 
 namespace QueryDesk
 {
