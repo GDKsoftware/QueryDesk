61b782dbae4600de435b8f7d3cd152d604827a3f
partouf <partouf@gmail.com>
2015-08-03 17:48:01 +0200

    #66 copy paste bug

--- QueryDesk/App.xaml.cs
+++ QueryDesk/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace QueryDesk
 {
@@ -13,5 +14,19 @@ namespace QueryDesk
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// http://stackoverflow.com/questions/12769264/openclipboard-failed-when-copy-pasting-data-from-wpf-datagrid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            var comException = e.Exception as System.Runtime.InteropServices.COMException;
+
+            if (comException != null && comException.ErrorCode == -2147221040)
+            {
+                e.Handled = true;
+            }
+        }
     }
 }
